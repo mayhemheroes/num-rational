166ae110aa65e196a8d1570197a140035fa96dd1
Jonathanzhao02 <zhaojonathan99@gmail.com>
2023-05-10 02:39:19 +0000

    Fixed add test

--- fuzz/fuzz_targets/add.rs
+++ fuzz/fuzz_targets/add.rs
@@ -3,7 +3,8 @@ use libfuzzer_sys::fuzz_target;
 use num_rational::Ratio;
 
 fuzz_target!(|input: (i32, i32, i32, i32)| {
-    if input.1 > 0 && input.3 > 0 {
-        let _ = Ratio::new(input.0, input.1) + Ratio::new(input.2, input.3);
+    let (a, b, c, d) = input;
+    if b >> 16 > 0 && d >> 16 > 0 {
+        let _ = Ratio::new(a >> 16, b >> 16) + Ratio::new(c >> 16, d >> 16);
     }
 });
